239e97c5bbc7c404ad450a16ecc964c4b4c83677
nezra <96aa07@gmail.com>
2019-05-28 10:30:36 -0400

    Create internet_hello.py

---
+++ examples/internet_hello.py
@@ -0,0 +1,61 @@
+
+import utime
+
+#uPy libraries
+
+from pyb import Pin, SPI
+
+import esp32spi
+import esp32spi_wifimanager
+import esp32spi_requests as requests
+
+esp32_cs = Pin("P3", Pin.OUT_OD) 
+esp32_ready = Pin("P7", Pin.IN) 
+esp32_reset = Pin("P8", Pin.OUT_PP) 
+spi = SPI(2,SPI.MASTER,baudrate=8000000,polarity=0,phase=0) 
+esp = esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset) 
+
+print("ESP32 SPI webclient test")
+
+TEXT_URL = "http://wifitest.adafruit.com/testwifi/index.html"
+JSON_URL = "http://api.coindesk.com/v1/bpi/currentprice/USD.json"
+
+requests.set_interface(esp)
+
+if esp.status == esp32spi.WL_IDLE_STATUS:
+    print("ESP32 found and in idle mode")
+print("Firmware vers.", esp.firmware_version)
+print("MAC addr:", [hex(i) for i in esp.MAC_address])
+
+for ap in esp.scan_networks():
+    print("\t%s\t\tRSSI: %d" % (str(ap['ssid'], 'utf-8'), ap['rssi']))
+
+print("Connecting to AP...")
+while not esp.is_connected:
+    try:
+        esp.connect_AP(b'YOUR_SSID', b'YOUR_PASSWORD')
+    except RuntimeError as e:
+        print("could not connect to AP, retrying: ",e)
+        continue
+print("Connected to", str(esp.ssid, 'utf-8'), "\tRSSI:", esp.rssi)
+print("My IP address is", esp.pretty_ip(esp.ip_address))
+print("IP lookup adafruit.com: %s" % esp.pretty_ip(esp.get_host_by_name("adafruit.com")))
+print("Ping google.com: %d ms" % esp.ping("google.com"))
+
+#esp._debug = True
+print("Fetching text from", TEXT_URL)
+r = requests.get(TEXT_URL)
+print('-'*40)
+print(r.text)
+print('-'*40)
+r.close()
+
+print()
+print("Fetching json from", JSON_URL)
+r = requests.get(JSON_URL)
+print('-'*40)
+print(r.json())
+print('-'*40)
+r.close()
+
+print("Done!")
