906fd12a1442290884e01f4ab7eb969dc8ceed28
nezra <96aa07@gmail.com>
2019-05-28 10:42:09 -0400

    Update esp32spi_cheerlights.py

--- examples/esp32spi_cheerlights.py
+++ examples/esp32spi_cheerlights.py
@@ -1,13 +1,10 @@
-import time
-import board
-import busio
-from digitalio import DigitalInOut
 
-from adafruit_esp32spi import adafruit_esp32spi
-from adafruit_esp32spi import adafruit_esp32spi_wifimanager
+import utime
 
-import neopixel
-import adafruit_fancyled.adafruit_fancyled as fancy
+#uPy libraries
+from pyb import Pin, SPI
+import esp32spi
+import esp32spi_wifimanager
 
 # Get wifi details and more from a secrets.py file
 try:
@@ -21,29 +18,21 @@ print("ESP32 SPI webclient test")
 DATA_SOURCE = "https://api.thingspeak.com/channels/1417/feeds.json?results=1"
 DATA_LOCATION = ["feeds", 0, "field2"]
 
-esp32_cs = DigitalInOut(board.D9)
-esp32_ready = DigitalInOut(board.D10)
-esp32_reset = DigitalInOut(board.D5)
-spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
-"""Use below for Most Boards"""
-status_light = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2) # Uncomment for Most Boards
-"""Uncomment below for ItsyBitsy M4"""
-#status_light = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.2)
-wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets, status_light)
+esp32_cs = Pin("P3", Pin.OUT_OD) 
+esp32_ready = Pin("P7", Pin.IN) 
+esp32_reset = Pin("P8", Pin.OUT_PP) 
+spi = SPI(2,SPI.MASTER,baudrate=8000000,polarity=0,phase=0) 
+esp = esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset,debug=True) 
 
-# neopixels
-pixels = neopixel.NeoPixel(board.A1, 16, brightness=0.3)
-pixels.fill(0)
+wifi = esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets)
 
 # we'll save the value in question
 last_value = value = None
-
 while True:
     try:
         print("Fetching json from", DATA_SOURCE)
         response = wifi.get(DATA_SOURCE)
-        print(response.json())
+        #print(response)# .json())
         value = response.json()
         for key in DATA_LOCATION:
             value = value[key]
@@ -66,4 +55,4 @@ while True:
         pixels.fill(gamma_corrected)
         last_value = value
     response = None
-    time.sleep(60)
+    utime.sleep(60)
